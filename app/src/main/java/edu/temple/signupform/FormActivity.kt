cfd0ec47dc070d6b7a1acb02ec9ff2012159704e
ArtlessBoi <jayanlas00@gmail.com>
2022-02-07 19:02:37 -0500

    added setEmail, setPassword, confirmPassword and button. when button is pressed, produces "Thanks for Signing up!" TODO has a few remaining things to do.

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -1,11 +1,17 @@
 package edu.temple.signupform
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //TODO(have button only work if all the criterias are met, which consist of: proper email, matching passwords and modify welcome message to say user's name)
+        val welcomeMessage = findViewById<TextView>(R.id.welcomeMessage)
+        findViewById<Button>(R.id.button).setOnClickListener{welcomeMessage.text = "Thanks For Signing Up!"}
     }
 }
